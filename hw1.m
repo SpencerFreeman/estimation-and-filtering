c712bdf466635d7f3e4a6806131098bb8fe5e4d1
Spencer Freeman <spencerfreeman1234@gmail.com>
2024-09-15 19:20:33 -0400

    finish hw1 p1-7

--- hw1.m
+++ hw1.m
@@ -28,6 +28,45 @@ x'*P*x
 
 
 
+%% 1-7
+clear;clc;close all
+
+n = 10000;
+
+nx = 2;
+ny = 5;
+
+nz = 3;
+
+xbar = randi(10, nx, 1);
+ybar = randi(10, ny, 1);
+
+sx = randi(10, nx, 1);
+sy = randi(10, ny, 1);
+
+x = randn(nx, n).*sx + xbar;
+y = randn(ny, n).*sy + ybar;
+
+A = randi(10, nz, nx);
+B = randi(10, nz, ny);
+C = randi(10, nz, 1);
+
+z = A*x + B*y + C;
+
+% 1
+zbar = mean(z')'
+zbar_true = A*xbar + B*ybar + C
+
+P = cov([x; y]');
+
+Pxx = P(1:nx,       1:nx);
+Pyy = P(nx + 1:end, nx + 1:end);
+Pxy = P(1:nx,       nx + 1:end);
+Pyx = Pxy';
+
+% 2
+Pz = cov(z')
+Pz_true = A*Pxx*A' + A*Pxy*B' + B*Pyx*A' + B*Pyy*B'
 
 
 
---
+++ test.m
@@ -0,0 +1,17 @@
+clear;clc;close all
+
+
+
+p0 = 9/10*pi;
+
+
+p1 = -9/10*pi;
+
+
+dp = wrapToPi(p1 - p0)
+
+
+
+
+
+
