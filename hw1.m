64cd6db6d38ed69269ebc4262a82e7c0c8d041c2
Spencer Freeman <spencerfreeman1234@gmail.com>
2024-09-20 12:35:40 -0400

    complete hw1

--- hw1.m
+++ hw1.m
@@ -69,6 +69,74 @@ Pz = cov(z')
 Pz_true = A*Pxx*A' + A*Pxy*B' + B*Pyx*A' + B*Pyy*B'
 
 
+%% 1-9
+clear;clc;close all
+
+alpha = .01;
+P = [1 .5; .5 1];
+
+Pinv = inv(P);
+
+e = [1; 1];
+
+Pe = Pinv * e;
+
+sig_beta = sqrt(Pe' * P * Pe); % variance of beta
+mu_beta  = 0;                  % mean of beta
+
+beta0 = -norminv(alpha/2, mu_beta, sig_beta); % threshold value
+
+
+% create sample measurements and assess the test
+thetas = linspace(-5, 5, 100);
+for i = 1:length(thetas)
+    theta = thetas(i);         % signal
+    m = 100;%100e3;                 % number of samples
+    w = mvnrnd([0; 0], P, m)'; % random draw noise terms
+    z = theta * e + w;         % noisy samples
+    b = z' * Pinv * e;         % test statistic for each sample
+
+    accept_H1 = abs(b) >= beta0; % test hypothesis
+
+    pw(i) = sum(accept_H1) / m; % detection rate (power)
+    Power(i) = ...
+        normcdf(-beta0,   (theta * e)' * Pinv * e, sig_beta) + ...
+        1-normcdf( beta0, (theta * e)' * Pinv * e, sig_beta);
+
+end
+
+
+figure
+plot(thetas, pw, thetas, Power)
+grid on
+legend('Observed', 'Theory')
+
+
+
+
+%%
+x = linspace(-5, 5, 100);
+y = normpdf(x, mu_beta, sig_beta);
+
+figure('WindowStyle', 'Docked')
+plot(x, y)
+grid on
+hold on
+xline(beta0)
+xline(-beta0)
+
+
+
+figure;plot(sort(accept_H1));grid on
+figure;plot(b);grid on
+figure;histogram(b)
+
+
+
+
+
+
+
 
 
 
