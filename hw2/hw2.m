c731b00416cc00086c6c3b8c14535dcdd16ea97c
Spencer Freeman <spencerfreeman1234@gmail.com>
2024-10-07 22:03:35 -0400

    complete hw2 problem 2-11

--- hw2/hw2.m
+++ hw2/hw2.m
@@ -27,12 +27,51 @@ n2 = 2*n;
 
 k_q0 = chi2inv(1 - alpha, n2) / 2;
 
-% q0 = k_q0 * 
+% q0 = k_q0 *
 
 Pmd = chi2cdf(2*k_q0/4, n2);
 
 
 fprintf('5: \n\tk_q0: %f\n\tPmd: %f\n', k_q0, Pmd)
+
+
+%% 2-11
+clear;clc;close all
+
+k = 1; % number of measurements
+
+sig = 10^2; % m
+d = 10^5 * ones(1, k); % m
+y = 10^3; % m
+
+% estimation
+J = -1/sig^2 * sum(d.^2 ./ (d.^2 + y^2) - 1);
+
+Jinv = 1/J;
+
+sig_yhat = sqrt(Jinv); % m
+
+n = 10000;
+yhats = nan(1, n);
+for i = 1:n
+    zs = sqrt(d(1)^2 + y^2) + sig*randn(1, k); % measurments, m
+
+    zbar = max(mean(zs), d(1)); % truncate values that cause problems...
+
+    yhat = sqrt(zbar^2 - d(1)^2); % MLE estimate, m
+
+    yhats(i) = yhat;
+end % for
+
+histfit(yhats) % plot histogram and fitted normal pdf
+pd = fitdist(yhats', 'Normal'); % return fitted normal pdf
+
+
+fprintf('2-11: \n\tTrue Altitude: %f (m)\n\tSTD of CRLB: %f (m)\n\tObserved Estimator STD: %f (m)\n\tObserved Estimator Mean: %f (m)\n', y, sig_yhat, pd.sigma, pd.mu)
+
+
+
+
 
 
 
