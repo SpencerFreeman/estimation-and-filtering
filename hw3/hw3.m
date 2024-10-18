266974b6d425e7763cd4aa8aa5df56faa9055e89
Spencer Freeman <spencerfreeman1234@gmail.com>
2024-10-18 13:47:24 -0400

    first cut at hw prob 5

--- hw3/hw3.m
+++ hw3/hw3.m
@@ -1,14 +1,22 @@
 clear;clc;close all
 
+% psi = x(1); % rad
+% y10 = x(2); % m
+% y20 = x(3); % m
+% psid = x(4); % rad/s
+% vr  = x(5); % m
 
+idispflag = true; % print outs
 
 load('radarmeasdata_cart.mat')
 
 rhobhist = rhobhist';
 rhoahist = rhoahist';
 thist = thist';
+k = length(thist); % number of samples
 
-% x = [ψ0; y1r0; y2r0;ψ;vr].
+nz = 2; % number of measurements
+nx = 5; % number of states
 
 la = -1; % m
 lb = 1; % m
@@ -29,18 +37,122 @@ v2z = diff(y2z) / (thist(2) - thist(1)); v2z = [v2z, v2z(end)];  % m/s
 
 y0 = [y1z(1); y2z(1)] - thist(1) * [v1z(1); v2z(1)]; % m
 
-% x = [y1z'; y2z'; v1z'; v2z'];
-
-% ψ0 = atan2(vr2,vr1) and vr = ||vr||.
 
 psi0 = atan2(v2z(1), v1z(1)); % rad
 psidot0 = 0; % rad/s
 vr0 = sqrt(v2z(1)^2 + v1z(1)^2); % m/s
 
-x = [psi0; y0(1); y0(2); psidot0; vr0]; % rad; m; m; rad/s; m/s
+x0 = [psi0; y0(1); y0(2); psidot0; vr0]; % rad; m; m; rad/s; m/s
 
 
-za = Rainv' * [rhoahist; rhobhist]; % measurements, m; m
+z = reshape( Rainv' * [rhoahist; rhobhist], nz*k, 1); % measurements stacked for batch processing, m; m
+
+
+
+
+%% processing loop
+
+xguess = x0;
+alpha = 1;
+
+[delx, J, dJdx, dJdalpha, d2Jdalpha2, P] = ...
+    newton_gauss(xguess, thist, z, Rainv, la, lb);
+
+delJpred = dJdalpha + .5*d2Jdalpha2; %  Predict the change in cost if a step size of alpha = 1 is taken.
+
+%
+%  Prepare some quantities for use in controlling the Gauss-Newton
+%  iterations.
+%
+testdone = 0;
+niteration = 0;
+iaflag = 0;
+
+tic
+while testdone == 0
+
+    alpha = 1;
+    xguessnew = xguess + alpha*delx;
+    [~, J_new, dJdx, dJdalpha, d2Jdalpha2, P] = ...
+        newton_gauss(xguess + alpha * delx, ...
+        thist, z, Rainv, la, lb);
+    %
+    %  Do step size halving if necessary in order to force a decrease
+    %  in the cost.
+    %
+    nalphahalf = 0;
+    while J_new >= J
+        nalphahalf = nalphahalf + 1;
+        if nalphahalf > 50
+            iaflag = 1;
+            break
+        end % if
+        alpha = 0.5*alpha;
+        xguessnew = xguess + alpha*delx;
+        [~, J_new, dJdx, dJdalpha, d2Jdalpha2, P] = ...
+            newton_gauss(xguess + alpha * delx, ...
+            thist, z, Rainv, la, lb);
+    end % while
+
+
+    % termination conditions
+    if iaflag == 1
+        itermflag = 1;
+        break
+    end % if
+    xguess = xguessnew;
+    Jold = J;
+    delJold = J_new - J;
+    delJpredold = delJpred;
+
+    [delx, J_new, dJdx, dJdalpha, d2Jdalpha2, P] = ...
+        newton_gauss(xguess, ...
+        thist, z, Rainv, la, lb);
+
+    delJpred = dJdalpha + .5*d2Jdalpha2;
+    delJsizetest = abs(delJpred) < 1.e-13*(1 + J);
+    delxsizetest = norm(delx) < 1.e-09*(1 + norm(xguess));
+    alphatest = alpha == 1;
+    delJratiotest = abs((delJold/delJpredold) - 1) < 0.01;
+    if delJsizetest & delxsizetest
+        testdone = 1;
+    end % if
+    if alphatest & delJratiotest & delxsizetest
+        testdone = 1;
+    end % if
+    niteration = niteration + 1;
+    if testdone == 0
+        if niteration >= 100
+            itermflag = 2;
+            testdone = 1;
+        end % if
+    end % if
+
+
+    if idispflag == 1
+        disp([' At iteration ',int2str(niteration),' alpha = ',...
+            num2str(alpha),', Jnew = ',num2str(J),', Jold = ',...
+            num2str(Jold),', and norm(delxnew) = ',...
+            num2str(norm(delx)),'.'])
+    end % if
+
+
+
+
+end % for
+toc
+
+xest = xguess;
+Jopt = J;
+
+
+
+
+
+
+
+
+
 
 
 
@@ -53,6 +165,7 @@ hold on
 plot(la, 0, 'ro')
 plot(lb, 0, 'bo')
 plot(y0(1), y0(2), 'x')
+plot(xest(2), xest(3), 'oc')
 
 grid on
 axis equal
@@ -61,21 +174,127 @@ axis equal
 
 
 
-% fprintf('5: \n\tk_q0: %f\n\tPmd: %f\n', k_q0, Pmd)
+fprintf('5: \n\tXest: %f %f %f %f %f\n\tJopt: %f\n', xest, Jopt)
 
 %%
-% z = ha([y1z; y2z], Rainv, la, lb);
+function [delx, J, dJdx, dJdalpha, d2Jdalpha2, P] = ...
+    newton_gauss(xguess, thist, z, Rainv, la, lb)
 
-function z = ha(x, Rainv, la, lb)
 
-z = Rainv' * ...
-    [vecnorm(x(2:3, :) - [la; 0]); ...
-     vecnorm(x(2:3, :) - [lb; 0])]; % m; m
+    H = dhdx_a(xguess, thist, Rainv, la, lb);
+    hg = h_a(xguess, thist, Rainv, la, lb);
+
+    [Qb, Rb] = qr(H, 0);
+
+    Rbinv = inv(Rb);
+
+    innov = z - hg; % estimator discrepancy
+
+    delx = Rbinv*Qb'*innov; % x step direction
+
+    J = 0.5*(innov'*innov); % cost function
+
+    P = Rbinv*Rbinv';
+    dJdx = H'*innov;
+    dJdalpha = dJdx'*delx;
+    dum = Rb*delx;
+    d2Jdalpha2 = dum'*dum;
 
 end % function
 
 
-% function J = 
+
+
+function za = h_a(x, t, Rainv, la, lb)
+
+nz = 2; % number of measurements
+k = length(t); % numnber of samples
+
+sa = nan(1, k);
+for i = 1:k
+    [sa(i), ~, ~] = safunct(.5*x(4)*t(i));
+end % for
+
+ys = [x(2) + x(5)*t.*sa.*cos(x(1) + .5*x(4)*t); ... % m
+      x(3) + x(5)*t.*sa.*sin(x(1) + .5*x(4)*t)]; % m
+
+za = reshape( Rainv' * ...
+    [vecnorm( ys - [la; 0] ); ...
+     vecnorm( ys - [lb; 0] )], nz*k, 1);
+
+end % function
+
+
+function Ha = dhdx_a(x, t, Rainv, la, lb)
+
+k = length(t);
+nx = length(x);
+nz = 2;
+
+Ha = nan(k*nz, nx);
+
+for i = 1:k
+
+    Ha((i - 1)*nz + (1:nz), :) = ...
+        Rainv' * ...
+        [drhodx(x, t(i), la); ...
+         drhodx(x, t(i), lb)];
+
+end % for
+
+
+end % function
+
+
+function dh = drhodx(x, t, l)
+
+theta = x(1) + .5*x(4)*t; % rad
+x5t = x(5)*t; % m
+ct = cos(theta);
+st = sin(theta);
+
+[sa, dsadx, ~] = safunct(.5*x(4)*t);
+dsadx4 = .5*t*dsadx;
+
+k0 = .5*( (x(2) + x5t*sa*ct - l)^2 + ...
+    (x(3) + x5t*sa*st    )^2 )^(-1/2);
+
+k1 = 2*(x(2) + x5t*sa*ct - l);
+k2 = 2*(x(3) + x5t*sa*st - l);
+
+dhdx1 = ...
+    ( x5t*sa*(-1)*st * k1 + ...
+    x5t*sa*     ct * k2) * k0;
+
+dhdx2 = (1)*k1*k0;
+
+dhdx3 = (1)*k2*k0;
+
+dhdx4 = ...
+    ( x5t*( dsadx4*ct + sa*.5*t*(-1)*st ) * k1 + ...
+    x5t*( dsadx4*st + sa*.5*t*     ct ) * k2 ) * k0;
+
+dhdx5 = ( t*sa*ct * k1 + t*sa*st * k2 ) * k0;
+
+dh = [dhdx1, dhdx2, dhdx3, dhdx4, dhdx5];
+
+end % function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- lecture17_matlabmcode_files/gnestmissle.m
+++ lecture17_matlabmcode_files/gnestmissle.m
@@ -1,12 +1,12 @@
 function [xest,Jopt,P,itermflag] = ...
-                     gnestmissle(thist,zhist,lradar,...
-                                 sigmarho,sigmatheta,idispflag)
+    gnestmissle(thist,zhist,lradar,...
+    sigmarho,sigmatheta,idispflag)
 %
-%  Copyright (c) 2002 Mark L. Psiaki.  All rights reserved.  
-% 
+%  Copyright (c) 2002 Mark L. Psiaki.  All rights reserved.
+%
 %  This function does Gauss-Newton estimation of the initial
 %  state of a missle in a missle tracking problem.
-%  
+%
 %
 %  Inputs:
 %
@@ -26,23 +26,23 @@ function [xest,Jopt,P,itermflag] = ...
 %    sigmatheta  The measurement error standard deviation of the
 %                radar elevation measurement, in radians.
 %
-%    idispflag   A flag that tells whether (idispflag = 1) or not 
+%    idispflag   A flag that tells whether (idispflag = 1) or not
 %                (idispflag = 0) to display interm optimization
 %                results during the Gauss-Newton iterations.
-%  
+%
 %  Outputs:
 %
-%    xest        = [y10est;v10est;y20est;v20est], the final 
-%                estimate of the vector of initial conditions of the 
-%                missle at time 0.  y10est is the estimated initial 
-%                horizontal position in meters, v10est is the estimated 
-%                initial horizontal velocity in m/sec, y20est is the 
-%                estimated initial vertical position in meters, and 
+%    xest        = [y10est;v10est;y20est;v20est], the final
+%                estimate of the vector of initial conditions of the
+%                missle at time 0.  y10est is the estimated initial
+%                horizontal position in meters, v10est is the estimated
+%                initial horizontal velocity in m/sec, y20est is the
+%                estimated initial vertical position in meters, and
 %                v20est is the estimated initial vertical velocity
 %                in m/sec.
 %
 %    Jopt        The final optimal value of the nonlinear least-squares
-%                cost.  Note that this is divided by 2 compared to the  
+%                cost.  Note that this is divided by 2 compared to the
 %                cost given in Bar-Shalom.
 %
 %    P           The 4x4 Gauss-Newton approximation of the estimation
@@ -67,114 +67,133 @@ function [xest,Jopt,P,itermflag] = ...
 %
 %  Get the number of radar measurements.
 %
-   k = size(thist,1);
+k = size(thist,1);
 %
 %  Make a first guess based on the first and last measurements
 %  and a belief that there is zero measurement error.
 %
-   rho_1 = zhist(1,1);
-   rho_k = zhist(k,1);
-   theta_1 = zhist(1,2);
-   theta_k = zhist(k,2);
-   t_1 = thist(1,1);
-   t_k = thist(k,1);
-   y1_1 = lradar - rho_1*cos(theta_1);
-   y1_k = lradar - rho_k*cos(theta_k);
-   y2_1 = rho_1*sin(theta_1);
-   y2_k = rho_k*sin(theta_k);
-   Hdum = [1 t_1 0 0;0 0 1 t_1;1 t_k 0 0;0 0 1 t_k];
-   delz = [y1_1;(y2_1 + 0.5*9.81*(t_1^2));y1_k;(y2_k + 0.5*9.81*(t_k^2))];
-   xguess = Hdum\delz;
+rho_1 = zhist(1,1);
+rho_k = zhist(k,1);
+theta_1 = zhist(1,2);
+theta_k = zhist(k,2);
+t_1 = thist(1,1);
+t_k = thist(k,1);
+y1_1 = lradar - rho_1*cos(theta_1);
+y1_k = lradar - rho_k*cos(theta_k);
+y2_1 = rho_1*sin(theta_1);
+y2_k = rho_k*sin(theta_k);
+Hdum = [1 t_1 0 0;0 0 1 t_1;1 t_k 0 0;0 0 1 t_k];
+delz = [y1_1;(y2_1 + 0.5*9.81*(t_1^2));y1_k;(y2_k + 0.5*9.81*(t_k^2))];
+xguess = Hdum\delz;
 %
 %  Calculate the initial cost and the initial Gauss-Newton search
 %  direction.
 %
-   [J,delxgn,dJdalpha,d2Jdalpha2,P,dJdx] = ...
-                     jdxgnmissle(xguess,thist,zhist,lradar,...
-                                 sigmarho,sigmatheta,1);
+[J,delxgn,dJdalpha,d2Jdalpha2,P,dJdx] = ...
+    jdxgnmissle(xguess,thist,zhist,lradar,...
+    sigmarho,sigmatheta,1);
 %
 %  Predict the change in cost if a step size of alpha = 1 is taken.
 %
-   delJpred = dJdalpha + .5*d2Jdalpha2;
+delJpred = dJdalpha + .5*d2Jdalpha2;
 %
 %  Decide whether to terminate now.
 %
-   delJsizetest = abs(delJpred) < 1.e-13*(1 + J);
-   delxsizetest = norm(delxgn) < 1.e-09*(1 + norm(xguess));
-   itermflag = 0;
-   if delJsizetest & delxsizetest
-      xest = xguess;
-      Jopt = J;
-      return
-   end
+delJsizetest = abs(delJpred) < 1.e-13*(1 + J);
+delxsizetest = norm(delxgn) < 1.e-09*(1 + norm(xguess));
+itermflag = 0;
+if delJsizetest & delxsizetest
+    xest = xguess;
+    Jopt = J;
+    return
+end % if
 %
 %  Prepare some quantities for use in controlling the Gauss-Newton
 %  iterations.
 %
-   testdone = 0;
-   niteration = 0;
-   iaflag = 0;
+testdone = 0;
+niteration = 0;
+iaflag = 0;
 %
 %  Do one Gauss-Newton iteration per iteration of the following while
 %  loop.
 %
-   while testdone == 0
-      alpha = 1;
-      xguessnew = xguess + alpha*delxgn;
-      Jnew = jdxgnmissle(xguessnew,thist,zhist,lradar,...
-                                 sigmarho,sigmatheta,0);
-%
-%  Do step size halving if necessary in order to force a decrease
-%  in the cost.
-%
-      nalphahalf = 0;
-      while Jnew >= J
-         nalphahalf = nalphahalf + 1;
-         if nalphahalf > 50
+while testdone == 0
+    alpha = 1;
+    xguessnew = xguess + alpha*delxgn;
+    Jnew = jdxgnmissle(xguessnew,thist,zhist,lradar,...
+        sigmarho,sigmatheta,0);
+    %
+    %  Do step size halving if necessary in order to force a decrease
+    %  in the cost.
+    %
+    nalphahalf = 0;
+    while Jnew >= J
+        nalphahalf = nalphahalf + 1;
+        if nalphahalf > 50
             iaflag = 1;
             break
-         end
-         alpha = 0.5*alpha;
-         xguessnew = xguess + alpha*delxgn;
-         Jnew = jdxgnmissle(xguessnew,thist,zhist,lradar,...
-                                 sigmarho,sigmatheta,0);
-      end
-      if iaflag == 1
-         itermflag = 1;
-         break
-      end
-      xguess = xguessnew;
-      Jold = J;
-      delJold = Jnew - J;
-      delJpredold = delJpred;
-      [J,delxgn,dJdalpha,d2Jdalpha2,P,dJdx] = ...
-                     jdxgnmissle(xguess,thist,zhist,lradar,...
-                                 sigmarho,sigmatheta,1);
-               
-      delJpred = dJdalpha + .5*d2Jdalpha2;
-      delJsizetest = abs(delJpred) < 1.e-13*(1 + J);
-      delxsizetest = norm(delxgn) < 1.e-09*(1 + norm(xguess));
-      alphatest = alpha == 1;
-      delJratiotest = abs((delJold/delJpredold) - 1) < 0.01;
-      if delJsizetest & delxsizetest
-         testdone = 1;
-      end
-      if alphatest & delJratiotest & delxsizetest
-         testdone = 1;
-      end
-      niteration = niteration + 1;                
-      if testdone == 0
-         if niteration >= 100
+        end % if
+        alpha = 0.5*alpha;
+        xguessnew = xguess + alpha*delxgn;
+        Jnew = jdxgnmissle(xguessnew,thist,zhist,lradar,...
+            sigmarho,sigmatheta,0);
+    end % while
+
+    if iaflag == 1
+        itermflag = 1;
+        break
+    end % if
+    xguess = xguessnew;
+    Jold = J;
+    delJold = Jnew - J;
+    delJpredold = delJpred;
+    [J,delxgn,dJdalpha,d2Jdalpha2,P,dJdx] = ...
+        jdxgnmissle(xguess,thist,zhist,lradar,...
+        sigmarho,sigmatheta,1);
+
+    delJpred = dJdalpha + .5*d2Jdalpha2;
+    delJsizetest = abs(delJpred) < 1.e-13*(1 + J);
+    delxsizetest = norm(delxgn) < 1.e-09*(1 + norm(xguess));
+    alphatest = alpha == 1;
+    delJratiotest = abs((delJold/delJpredold) - 1) < 0.01;
+    if delJsizetest & delxsizetest
+        testdone = 1;
+    end % if
+    if alphatest & delJratiotest & delxsizetest
+        testdone = 1;
+    end % if
+    niteration = niteration + 1;
+    if testdone == 0
+        if niteration >= 100
             itermflag = 2;
             testdone = 1;
-         end
-      end
-      if idispflag == 1
-         disp([' At iteration ',int2str(niteration),' alpha = ',...
-                num2str(alpha),', Jnew = ',num2str(J),', Jold = ',...
-                num2str(Jold),', and norm(delxnew) = ',...
-                num2str(norm(delxgn)),'.'])
-      end
-   end
-   xest = xguess;
-   Jopt = J;   
+        end % if
+    end % if
+    if idispflag == 1
+        disp([' At iteration ',int2str(niteration),' alpha = ',...
+            num2str(alpha),', Jnew = ',num2str(J),', Jold = ',...
+            num2str(Jold),', and norm(delxnew) = ',...
+            num2str(norm(delxgn)),'.'])
+    end % if
+    
+end % while
+xest = xguess;
+Jopt = J;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
